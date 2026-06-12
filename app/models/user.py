e1c3eb6e58e307a0f58744b29c9b44f30f4a3451
many-to-one <xllxlex@gmail.com>
2026-06-12 08:42:34 +0200

    null hashed psw for google ouath

--- app/models/user.py
+++ app/models/user.py
@@ -8,7 +8,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     name = Column(String, nullable=True)
-    hashed_password = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=True)
     is_active = Column(Boolean, default=True)
     full_name = Column(String)
     reset_token = Column(String, nullable=True)
