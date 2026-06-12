b22635bac9e5fe22e214dfa59bd5c59ff07c5e54
many-to-one <xllxlex@gmail.com>
2026-06-12 08:11:44 +0200

    name: Optional[str]

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -8,7 +8,7 @@ class UserBase(BaseModel):
 class UserCreate(BaseModel):
     email: EmailStr
     password: str = Field(..., min_length=6, max_length=128)
-    name: str | None = None,
+    name: Optional[str] = None
 
     @validator("password")
     def validate_password(cls, v):
